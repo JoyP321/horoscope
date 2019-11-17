46de6a948594e06f42b2aba548d86707b25d04ca
JoyP321 <54326509+JoyP321@users.noreply.github.com>
2019-11-17 14:59:33 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -27,7 +27,7 @@ def render_page1_response():
 
 def get_horoscope(fortunes, sign):
   date = datetime.datetime.now()
-  return "Hey " + sign + "! Your " + date.strftime("%A")+ " horoscope is: \n" +fortunes[sign][ date.strftime("%A")] 
+  return "Hey " + sign + "! Your " + date.strftime("%A")+ " horoscope is: \n" +fortunes[sign]["Monday"] 
   
 
 if __name__=="__main__":
