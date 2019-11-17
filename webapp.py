483f48c095ec95231da95571ccb89983afcdcbd4
JoyP321 <54326509+JoyP321@users.noreply.github.com>
2019-11-16 19:40:43 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,18 +1,25 @@
 from flask import Flask, url_for, render_template, request
-
+import os
+import json
 app = Flask(__name__)
 
 @app.route("/")
 def render_main():
+   with open('horoscope_data.json') as demographics_data:
+    counties = json.load(demographics_data)
   return render_template('home.html')
 
 @app.route("/p1")
 def render_page1():
+  with open('horoscope_data.json') as demographics_data:
+    counties = json.load(demographics_data)
   return render_template('page1.html', horoscope = "")
 
 
 @app.route("/response")
 def render_page1_response():
+  with open('horoscope_data.json') as demographics_data:
+    counties = json.load(demographics_data)
   sign = request.args['sign']
   return render_template('page1.html', horoscope = sign)
 
