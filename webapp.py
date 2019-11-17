56ba8642580c719bf2d58da21399aed15a24533b
JoyP321 <54326509+JoyP321@users.noreply.github.com>
2019-11-16 19:52:44 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -5,23 +5,27 @@ app = Flask(__name__)
 
 @app.route("/")
 def render_main():
-  with open('horoscope_data.json') as demographics_data:
-    counties = json.load(demographics_data)
+  with open('horoscope_data.json') as horoscope_data:
+    fortunes = json.load(horoscope_data)
   return render_template('home.html')
 
 @app.route("/p1")
 def render_page1():
-  with open('horoscope_data.json') as demographics_data:
-    counties = json.load(demographics_data)
+  with open('horoscope_data.json') as horoscope_data:
+    fortunes = json.load(horoscope_data)
   return render_template('page1.html', horoscope = "")
 
 
 @app.route("/response")
 def render_page1_response():
-  with open('horoscope_data.json') as demographics_data:
-    counties = json.load(demographics_data)
+  with open('horoscope_data.json') as horoscope_data:
+    fortunes = json.load(horoscope_data)
   sign = request.args['sign']
   return render_template('page1.html', horoscope = sign)
+
+
+
+  
 
 if __name__=="__main__":
   app.run(debug=True)
